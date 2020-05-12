49592e4eefc702f8f3019c2835a4b4bda6f081cd
Hung PT11 <phamthehungtb91@gmail.com>
2020-05-12 22:20:38 +0700

    Genarate  Question model

---
+++ app/Question.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Str;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    protected $fillable = ['title', 'body'];
+    //
+    public function User()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+}
