2925f1df6efb4c2b1bb3dbd70387bc6616a72478
hungpt11 <phamthehungtb91@gmail.com>
2020-05-15 17:58:39 +0700

    delete question

--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -33,11 +33,19 @@
                         <div class="media-body">
                             <div class="d-flex  align-items-center">
                                 <h3 class="mt-0">
-                                <a href="{{ $question->url }}"> {{$question->title}}</a>
+                                    <a href="{{ $question->url }}"> {{$question->title}}</a>
                                 </h3>
                                 <div class="ml-auto">
-                                    <a class="btn btn-sm btn-outline-info"
-                                        href="{{route('question.edit', $question->id)}}">Edit</a>
+                                    <div class="d-flex justify-content-between">
+                                        <a class="btn btn-sm btn-outline-info"
+                                            href="{{route('question.edit', $question->id)}}">Edit</a>
+                                        <form style="margin-left: 10px" action="{{route('question.destroy', $question->id)}}"
+                                            method="POST">
+                                            {{method_field('DELETE')}}
+                                            @csrf
+                                            <button type="submit" onclick="return confirm('Are you sure')" class="btn btn-sm btn-outline-danger">Delete</button>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
                             <p class="lead">
